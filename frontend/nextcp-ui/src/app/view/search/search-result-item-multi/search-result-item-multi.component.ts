9c31ab26c7bdf36f6dc3dff2335322d92b3623b7
sf666 <satfreak666@gmx.de>
2020-11-10 12:55:50 +0100

    UI: table styling moved to central place

--- frontend/nextcp-ui/src/app/view/search/search-result-item-multi/search-result-item-multi.component.ts
+++ frontend/nextcp-ui/src/app/view/search/search-result-item-multi/search-result-item-multi.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { ContentDirectoryService } from './../../../service/content-directory.service';
+import { ContainerDto } from './../../../service/dto.d';
 import { SearchItemService } from './../../../service/search/search-item.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,5 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultItemMultiComponent {
 
-  constructor(public searchItemService: SearchItemService) { }
+  constructor(
+    public searchItemService: SearchItemService,
+    private contentDirectoryService: ContentDirectoryService,
+    private router: Router) { 
+
+  }
+
+  browseToContainer(container: ContainerDto) {
+    this.contentDirectoryService.browseChildrenByContiner(container);
+    this.router.navigateByUrl('music-library');    
+  }
 }
