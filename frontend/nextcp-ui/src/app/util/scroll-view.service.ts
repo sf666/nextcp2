99997068064a932ac55521983156d6c50ddcf6b3
sf666 <satfreak666@gmx.de>
2020-11-12 16:14:16 +0100

    UI: temporary fix for button clicks

--- frontend/nextcp-ui/src/app/util/scroll-view.service.ts
+++ frontend/nextcp-ui/src/app/util/scroll-view.service.ts
@@ -13,7 +13,9 @@ export class ScrollViewService {
    * @param elementID ATTENTION: elementID needs to have tabindex set: <div id="elementID" tabindex="-1">
    */
   public scrollIntoViewID(elementID: string): void {
+    return;
 
+    // buttons don't work any more ...
     let targetElement = document.getElementById(elementID); // querySelector('#someElementId');
 
     if (targetElement) {
