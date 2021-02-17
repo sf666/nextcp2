9c232af2647aa8ad25dfacf47b2337d78828fdd4
sf666 <satfreak666@gmx.de>
2021-02-17 10:24:52 +0100

    Added UMS server support

--- backend/nextcp2-runtime/src/main/java/nextcp/upnp/device/mediaserver/UmsServerDevice.java
+++ backend/nextcp2-runtime/src/main/java/nextcp/upnp/device/mediaserver/UmsServerDevice.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import org.fourthline.cling.model.meta.RemoteDevice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import nextcp.dto.Config;
 import okhttp3.Call;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -19,6 +21,10 @@ public class UmsServerDevice extends MediaServerDevice
     private static final Logger log = LoggerFactory.getLogger(UmsServerDevice.class.getName());
     private OkHttpClient okClient = new OkHttpClient.Builder().build();
 
+    @Autowired
+    private Config config = null;
+    
+    
     public UmsServerDevice(RemoteDevice device)
     {
         super(device);
@@ -27,40 +33,26 @@ public class UmsServerDevice extends MediaServerDevice
     @Override
     public void rescan()
     {
-        String fileChanged = "C:\\test/Folder with space /test.mp3";
-        RequestBody body = RequestBody.create(fileChanged, MediaType.parse("application/text"));
         String requestUrl = String.format("%sapi/rescan", getDevice().getDetails().getBaseURL());
-        Request request = new Request.Builder().url(requestUrl).post(body).build();
+        Request request = new Request.Builder().url(requestUrl).get().build();
         Call call = okClient.newCall(request);
         try
         {
             Response response = call.execute();
+            // read status status code
         }
         catch (IOException e)
         {
             log.warn("unable to retrieve rating data from musicbrainz.org", e);
         }
-
-        //        String requestUrl = String.format("%sconsole/scan", getDevice().getDetails().getBaseURL());
-//        Request request = new Request.Builder().url(requestUrl).get().build();
-//        Call call = okClient.newCall(request);
-//        try
-//        {
-//            Response response = call.execute();
-//        }
-//        catch (IOException e)
-//        {
-//            log.warn("unable to retrieve rating data from musicbrainz.org", e);
-//        }
     }
     
     @Override
     public void rescanFile(File f)
     {
-        String fileChanged = "test";
-        RequestBody body = RequestBody.create(fileChanged, MediaType.parse("application/text"));
-        String requestUrl = String.format("%sapi/rescan", getDevice().getDetails().getBaseURL());
-        Request request = new Request.Builder().url(requestUrl).post(body).build();
+        RequestBody body = RequestBody.create(config.localIndexerSupport.musicRootPath, MediaType.parse("application/text"));
+        String requestUrl = String.format("%sapi/rescanFileOrFolder", getDevice().getDetails().getBaseURL());
+        Request request = new Request.Builder().url(requestUrl).addHeader("api-key","123456789012").post(body).build();
         Call call = okClient.newCall(request);
         try
         {
