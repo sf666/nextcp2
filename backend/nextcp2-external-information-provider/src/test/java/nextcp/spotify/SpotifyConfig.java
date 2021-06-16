c0d1061edb225b8b31a4c9a6e7221e5c80e0b864
sf666 <satfreak666@gmx.de>
2021-06-16 15:44:34 +0200

    Spotify config bean added

---
+++ backend/nextcp2-external-information-provider/src/test/java/nextcp/spotify/SpotifyConfig.java
@@ -0,0 +1,28 @@
+package nextcp.spotify;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpotifyConfig implements ISpotifyConfig
+{
+
+    public SpotifyConfig()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getClientId()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getSpotifyRefreshToken()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
