f94fcaaeb7cd9f5fe3fbb3f196bcb8e2fc5f43ef
sf666 <satfreak666@gmx.de>
2022-04-06 14:12:19 +0200

    changed standby service logic

--- backend/nextcp2-runtime/src/main/java/nextcp/rest/RestSimpleDeviceControl.java
+++ backend/nextcp2-runtime/src/main/java/nextcp/rest/RestSimpleDeviceControl.java
@@ -2,13 +2,11 @@ package nextcp.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import nextcp.upnp.device.mediarenderer.MediaRendererDevice;
 
@@ -26,26 +24,10 @@ public class RestSimpleDeviceControl extends BaseRestService
      *            ON / OFF
      */
     @GetMapping("/standby/{mediaRendererDevice}/{state}")
-    public String standby(@PathVariable("mediaRendererDevice") String udn, @PathVariable("state") String newState)
+    public String standby(@PathVariable("mediaRendererDevice") String udn, @PathVariable("state") boolean standby)
     {
-        boolean standby = false;
-        if ("on".equals(newState.toLowerCase()))
-        {
-            standby = false;
-        }
-        else if ("off".equals(newState.toLowerCase()))
-        {
-            standby = true;
-        }
-        else
-        {
-            log.warn("Illegal request state : " + newState);
-            throw new ResponseStatusException(HttpStatus.EXPECTATION_FAILED, "Illegal request state. Should be ON or OFF. Received : " + newState);
-        }
-
         log.info(String.format("Setting standby state on device %s to : %s", udn, Boolean.toString(standby)));
         MediaRendererDevice device = getMediaRendererByUdn(udn);
-
         if (standby)
         {
             device.getPlaylistServiceBridge().pause();
@@ -57,8 +39,7 @@ public class RestSimpleDeviceControl extends BaseRestService
     /**
      * 
      * @param udn
-     * @param newState
-     *            ON / OFF
+     * @return Standby state 
      */
     @GetMapping("/standby/{mediaRendererDevice}")
     public Boolean standby(@PathVariable("mediaRendererDevice") String udn)
