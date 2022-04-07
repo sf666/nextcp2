ff1538b42ff162ca40fb4252a0e831183717c3da
sf666 <satfreak666@gmx.de>
2022-04-07 15:00:11 +0200

    added debug log

--- backend/nextcp2-runtime/src/main/java/nextcp/rest/RestSimpleDeviceControl.java
+++ backend/nextcp2-runtime/src/main/java/nextcp/rest/RestSimpleDeviceControl.java
@@ -61,13 +61,16 @@ public class RestSimpleDeviceControl extends BaseRestService
         Optional<MusicItemDto> radio = device.getRadioServiceBridge().getRadioStations().stream().filter(mi -> mi.title.toLowerCase().startsWith(station)).findFirst();
         if (radio.isPresent())
         {
+            log.info("plauing radio ... " + station);
             device.getRadioServiceBridge().play(radio.get());
         }
+        log.warn("radio station not found : " + station);
     }
 
     @GetMapping("/pauseRadio/{mediaRendererDevice}")
     public void pauseRadio(@PathVariable("mediaRendererDevice") String udn)
     {
+        log.info("pause radio");
         MediaRendererDevice device = getMediaRendererByUdn(udn);
         device.getRadioServiceBridge().pause();
     }
