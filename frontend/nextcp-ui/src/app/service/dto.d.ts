8b5ca1b513ca2f17ed542d1babfddb29753d9316
sf666 <satfreak666@gmx.de>
2022-07-13 08:13:02 +0200

    prepare next release

--- frontend/nextcp-ui/src/app/service/dto.d.ts
+++ frontend/nextcp-ui/src/app/service/dto.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.32.889 on 2022-07-13 07:57:10.
+// Generated using typescript-generator version 2.32.889 on 2022-07-13 08:09:37.
 
 export interface ApplicationConfig {
     generateUpnpCode: boolean;
