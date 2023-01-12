e2dfa05c8e6daf69318943085a501e6f120dfe08
sf666 <satfreak666@gmx.de>
2023-01-12 10:08:09 +0100

    catch NPE in version text file cannot be found

--- backend/nextcp2-runtime/src/main/java/nextcp/util/CurrentVersion.java
+++ backend/nextcp2-runtime/src/main/java/nextcp/util/CurrentVersion.java
@@ -1,17 +1,26 @@
 package nextcp.util;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
-public class CurrentVersion {
+public class CurrentVersion
+{
 
-	public String CURRENT_VERSION;
+    public String CURRENT_VERSION;
 
-	public CurrentVersion() {
-		CURRENT_VERSION = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/version.txt"), StandardCharsets.UTF_8))
-			.lines().collect(Collectors.joining("\n"));
-	}
-	
+    public CurrentVersion()
+    {
+        InputStream is_version = getClass().getResourceAsStream("/version.txt");
+        if (is_version != null)
+        {
+            CURRENT_VERSION = new BufferedReader(new InputStreamReader(is_version, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+        }
+        else
+        {
+            CURRENT_VERSION = "UNKNOWN";
+        }
+    }
 }
