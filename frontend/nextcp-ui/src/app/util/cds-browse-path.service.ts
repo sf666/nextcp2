941d0451c356b8603f055150bf017291e4809ec0
sf666 <satfreak666@gmx.de>
2023-11-27 13:55:13 +0100

    disable cds browse path service. not needed any more with UMS

--- frontend/nextcp-ui/src/app/util/cds-browse-path.service.ts
+++ frontend/nextcp-ui/src/app/util/cds-browse-path.service.ts
@@ -7,56 +7,27 @@ import Stack from 'ts-data.stack';
 
 export class CdsBrowsePathService {
 
-  private visitedPathFromRoot: Stack<string>;
-  private visitedPathFromRootAsString: string;
   private scrollId: string;
-  private uniqueId: string;
 
   constructor(@Inject('uniqueId') private uniqueId_: string) {
-    this.uniqueId = uniqueId_;
-    this.visitedPathFromRoot = new Stack();
     this.scrollId = 'ID_SCROLL_TO_ELEMENT_STEP_IN';
-    this.visitedPathFromRootAsString = '';
-    if (uniqueId_?.length == 0) {
-      console.error("provide uniqueId");
-    }
   }
 
   public stepIn(path: string): void {
-    this.visitedPathFromRoot.push(path);
-    this.addPathToString(path);
     this.scrollId = 'ID_SCROLL_TO_ELEMENT_STEP_IN';
   }
 
   private addPathToString(path: string) {
-    if (this.visitedPathFromRootAsString.length > 0) {
-      this.visitedPathFromRootAsString = this.visitedPathFromRootAsString.concat(',');
-    }
-    this.visitedPathFromRootAsString = this.visitedPathFromRootAsString.concat(path);
-
   }
 
   public stepOut(): void {
-    if (this.visitedPathFromRoot.peek()) {
-      this.scrollId = this.visitedPathFromRoot.pop();
-      this.visitedPathFromRootAsString = this.visitedPathFromRootAsString.substring(0, this.visitedPathFromRootAsString.lastIndexOf(','));
-    }
   }
 
   public peekCurrentPathID(): string {
-    if (this.visitedPathFromRoot.isEmpty()) {
-      return '';
-    }
-
-    return this.visitedPathFromRoot.peek();
+    return "DEPRECATED";
   }
 
   public clearPath(): void {
-    while (!this.visitedPathFromRoot.isEmpty) {
-      this.visitedPathFromRoot.pop();
-    }
-    this.visitedPathFromRootAsString = '';
-    this.persistPathToRoot();
   }
 
   get scrollToID(): string {
@@ -64,20 +35,9 @@ export class CdsBrowsePathService {
   }
 
   public persistPathToRoot(): void {
-    localStorage.setItem(this.uniqueId + 'visitedPath', this.visitedPathFromRootAsString);
   }
 
   public restorePathToRoot(): void {
-    const pathStack = localStorage.getItem(this.uniqueId + 'visitedPath');
-    this.clearPath();
-    if (pathStack) {
-      pathStack.split(',').forEach(p => {
-        this.visitedPathFromRoot.push(p);
-        this.addPathToString(p);
-      });
-    }
-    this.persistPathToRoot();
-    console.log('path restored');
   }
 
   /**
