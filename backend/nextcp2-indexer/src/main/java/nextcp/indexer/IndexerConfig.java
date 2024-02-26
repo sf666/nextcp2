d5826b1e4d5331f39aa137fa9d10e0b483e01889
sf666 <satfreak666@gmx.de>
2024-02-26 10:54:03 +0100

    removed indexer package

--- backend/nextcp2-indexer/src/main/java/nextcp/indexer/IndexerConfig.java
+++
@@ -1,9 +0,0 @@
-package nextcp.indexer;
-
-public class IndexerConfig
-{
-    public Boolean isActive = false;
-    public String musicDirectory = "";
-    public String supportedFileTypes = "flac";
-    public String playlistDirectory = "";
-}
