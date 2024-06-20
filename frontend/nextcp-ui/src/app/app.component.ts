f7b69b23fcaee4091f9c7c472fcb6a08395187a7
sf666 <satfreak666@gmx.de>
2024-06-20 10:01:34 +0200

    cleanup app-component

--- frontend/nextcp-ui/src/app/app.component.ts
+++ frontend/nextcp-ui/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { SpinnerService } from './service/spinner.service';
 import { LayoutService } from './service/layout.service';
-import { Component, HostListener } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import * as _ from "lodash";
@@ -15,21 +15,15 @@ import { RouterOutlet } from '@angular/router';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
     imports: [RouterOutlet, MatProgressSpinner, SidebarComponent, FooterComponent, AsyncPipe]
 })
 export class AppComponent {
   title = 'nextCP/2';
 
-  opened: boolean;
-  events: string[] = [];
-
-  showFiller = false;
-
-//  private throttleResize = _.throttle(this.resiseVh, 100);
-
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, public layoutService: LayoutService, public spinnerService: SpinnerService) {
-    // SVG icon set was removed. This is for futere documentation, how to register icon sets
+    // SVG icon set was removed. This is for future documentation, how to register icon sets
     //
     // Globally register SVG mat-icon 
     // iconRegistry.addSvgIconSet(sanitizer.bypassSecurityTrustResourceUrl('assets/icon-set.svg'));
@@ -43,15 +37,16 @@ export class AppComponent {
     }
   }
 
+  /*
   // Mobile Devices URL bar / view height fix.
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     // this.throttleResize();
     // this.resiseVh();
   }
-
   private resiseVh() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
+*/
 }
