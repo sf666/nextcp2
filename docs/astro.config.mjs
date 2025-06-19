937968ecae074e8ed84aca588fbdf6a48acebf04
sf666 <satfreak666@gmx.de>
2025-06-19 14:47:13 +0200

    added documentation site

---
+++ docs/astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [starlight({
+		title: 'NextCP/2 Documentation',
+		head: [
+			{ tag: 'script',
+				attrs: {
+					src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.js',
+					crossorigin: "anonymous",
+				},
+			},
+			{ tag: 'link',
+				attrs: {
+					href: '', // https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.css
+					rel: "stylesheet",
+					crossorigin: "anonymous",
+				},
+			},
+		],
+		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/sf666/nextcp2' }],
+		sidebar: [
+			{ label: 'Installation', autogenerate: { directory: '/quick_install' }, },
+			{ label: 'User Interface', autogenerate: { directory: '/user_interface' }, },
+		],
+	}), react()],
+});
